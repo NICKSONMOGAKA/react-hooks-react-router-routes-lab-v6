6f6453aa4b740de743f3139010595f6704374d29
nicksonmogaka <nickson.mogaka@student.moringaschool.com>
2024-08-18 13:20:48 +0300

    Update Movie.js

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,37 +1,25 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
-import NavBar from "../components/NavBar";
-
-
-const BaseUrl='http://localhost:4000/movies'
+import { useEffect, useState } from 'react';
+import NavBar from '../components/NavBar';
+import { useParams } from 'react-router-dom';
 
 function Movie() {
-  const [movie, setMovie] = useState({})
-  const params = useParams();
-  const movieId = params.id
-
-  useEffect(() =>{
-    fetch(`${BaseUrl}/${movieId}`)
-    .then(r => r.json())
-    .then(data => setMovie(data))
-    .catch(error => console.error(error))
-  }, [movieId])
-  
-  if(!movie.title){
-    return <h1>Loading...</h1>
-  }
-
-  const genres = movie.genres.map(genre => <span key={genre}>{genre}</span>)
-
+  const [movie, setMovie] = useState({});
+  const { id } = useParams();
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${id}`)
+      .then((response) => response.json())
+      .then((movie) => setMovie(movie));
+  }, [id]);
   return (
     <>
       <header>
         <NavBar />
       </header>
       <main>
-        <h1>{movie.title}</h1>
-        <p>{movie.time}</p>
-        {genres}
+        <article>
+          <h1>{movie.title}</h1>
+          <p>{movie.time}</p>
+        </article>
       </main>
     </>
   );
